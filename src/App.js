737d6729174023c5a35f6a9a274ed3819e7454b3
Pavan Kumar <pavankumarmaddala585@gmail.com>
2022-04-20 13:55:10 +0530

    Better implementation.

--- src/App.js
+++ src/App.js
@@ -7,31 +7,65 @@ function App() {
   const [currentQuestion, setcurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [countScore, setCountScore] = useState(0);
+  const [startQuiz, setStartQuiz] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
   useEffect(() => {
     setData(questions);
   }, []);
 
   const handlecurrentQuestion = (isCorrect) => {
-    if (isCorrect) setCountScore(countScore + 1);
+    setIsSelected(true);
 
-    if (currentQuestion < data.length - 1)
-      setcurrentQuestion(currentQuestion + 1);
-    else setShowScore(true);
+    if (isCorrect) setCountScore(countScore + 1);
   };
+
+  const handleSubmit = () => {
+    if (currentQuestion < data.length - 1 && isSelected)
+      setcurrentQuestion(currentQuestion + 1);
+    if (currentQuestion === data.length - 1) setShowScore(true);
+  };
+
+  const handleRestart = () => {
+    setcurrentQuestion(0);
+    setCountScore(0);
+    setShowScore(false);
+  };
+
+  const handleStartQuiz = () => {
+    setStartQuiz(true);
+  };
+
+  if (!startQuiz)
+    return (
+      <div className="quiz">
+        <div className="score">
+          <button onClick={handleStartQuiz}>Start Quiz</button>
+          <div className="quiz-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+            rerum nesciunt hic temporibus consequatur eius eum blanditiis.
+            Minima esse iste sequi reprehenderit optio! Eum, laudantium
+            exercitationem? Dolorum officiis voluptatum ipsam?
+          </div>
+        </div>
+      </div>
+    );
 
   return (
     <main className="quiz">
       {showScore ? (
-        <p>
-          You scored {countScore} out of {data.length}
-        </p>
+        <div className="score">
+          <p>
+            You scored {countScore} out of {data.length}
+          </p>
+          <button onClick={handleRestart}>Restart Quiz</button>
+        </div>
       ) : (
         <React.Fragment>
           <section className="quiz-question">
-            <p>
+            <h1>
               Question {currentQuestion + 1} / {data.length}
-            </p>
+            </h1>
             <p>{data[currentQuestion] && data[currentQuestion].questionText}</p>
           </section>
           <section className="quiz-options">
@@ -45,6 +79,9 @@ function App() {
                   {option.answerText}
                 </button>
               ))}
+            <button onClick={handleSubmit} className="submit-btn">
+              Submit
+            </button>
           </section>
         </React.Fragment>
       )}
